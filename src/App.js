d600f8fe88e490b4959cf8e01cdf0783399dcbef
Rens van der Velden <I491926@fhict.nl>
2024-03-27 11:39:19 +0100

    React router DOM en Bootstrap toegevoegd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import FeedbackList from "./Components/FeedbackList";
+import {Route, Routes} from "react-router-dom";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <FeedbackList />
+        <NavBar></NavBar>
+        <Routes>
+            <Route path="/mijnfeedback" element={<FeedbackList />} />
+            <Route path="/mijnteams" element={<FeedbackList />} />
+            <Route path="/feedbackgeven" element={<FeedbackList />} />
+        </Routes>
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,28 @@
+import {Link} from "react-router-dom";
+
+export default function NavBar() {
+    return (
+        <div>
+            <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme={"dark"}>
+                <div className="container-fluid">
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to={"/home"} className={"nav-link active" }>Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={"/mijnfeedback"} className={"nav-link"}>Mijn teams</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={"/mijnteams"} className={"nav-link"}>Mijn feedback</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={"/feedbackgeven"} className={"nav-link"}>Feedback geven</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
