edbf632350042bea571d0fa63fb219aaaf506972
Amin El Abdellaoui <i467072@fhict.nl>
2024-04-17 14:56:32 +0200

    Kan alle groepfeedback halen (Admin/teamleider functie)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import {Route, Routes} from "react-router-dom";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import MijnFeedback from "./Pages/MijnFeedback";
 import FeedbackGeven from "./Pages/FeedbackGeven";
 import MijnTeams from "./Pages/MijnTeams";
+import AlleGroepFeedback from "./Pages/AlleGroepFeedback";
 
 function App() {
   return (
     <div className="App">
-        <NavBar></NavBar>
-        <Routes>
-            <Route path="/feedback" element={<MijnFeedback />} />
-            <Route path="/teams" element={<MijnTeams />} />
-            <Route path="/feedbackgeven" element={<FeedbackGeven />} />
-        </Routes>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/feedback" element={<MijnFeedback />} />
+        <Route path="/teams" element={<MijnTeams />} />
+        <Route path="/feedbackgeven" element={<FeedbackGeven />} />
+        <Route path="/AlleGroepFeedback" element={<AlleGroepFeedback />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/AlleGroepFeedback.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import EenFeedbackObjectZien from "../Components/EenFeedbackObjectZien";
+import AlleGroepFeedbackLijst from "../Components/AlleGroepFeedbackLijst";
+
+export default function AlleGroepFeedback() {
+  const [geselecteerdeFeedback, setGeselecteerdeFeedback] = useState(null);
+
+  const handleClick = (feedback) => {
+    //Bij het aanklikken van een object wordt de usestate geupdate naar het geklikte object
+    setGeselecteerdeFeedback(feedback);
+  };
+
+  return (
+    <div className={"container"}>
+      <div className={"row"}>
+        <div className={"col-1"}></div>
+        <div className={"col-4 gy-2"}>
+          {/*De onclick wordt als property meegegeven aan MijnFeedbackLijst */}
+          <AlleGroepFeedbackLijst onClick={handleClick} />
+        </div>
+        <div className={"col-1"}></div>
+        <div className={"col-6 gy-2"} style={{ height: "90vh" }}>
+          <div className={"h-100"}>
+            {/*Het geselecteerde object vanuit MijnFeedbackLijst wordt meegegeven als property */}
+            <EenFeedbackObjectZien geselecteerdeFeedback={geselecteerdeFeedback} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
