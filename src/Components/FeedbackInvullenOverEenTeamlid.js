fd10da7399e3a4085b30880f6c1f958e2fed9b5e
Rens van der Velden <I491926@fhict.nl>
2024-04-17 15:39:51 +0200

    post naar backend gelukt

--- src/Components/FeedbackInvullenOverEenTeamlid.js
+++ src/Components/FeedbackInvullenOverEenTeamlid.js
@@ -1,6 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 export default function FeedbackInvullenOverEenTeamlid({geselecteerdTeamlid}) {
+    const [feedbackText, setFeedbackText] = useState("");
+    const [feedback, setFeedback] = useState({
+        givenFeedback: '',
+        actief: true,
+        zender: [
+            {
+                id: 2
+            }
+        ],
+        ontvanger: [
+            {
+                id: 1
+            }
+        ]
+    });
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+    };
+
+    const handleSubmit = () => {
+        const feedbackData = {
+            givenFeedback: feedbackText,
+            actief: true,
+            zender: {
+                    id: 2
+                },
+            ontvanger: {
+                    id: 1
+                }
+        };
+        console.log(feedbackData);
+        axios.post("https://localhost:7145/feedbackAPI/Feedback/maakfeedback", feedbackData)
+            .then(response => {
+                console.log(response.status, response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
     //Er wordt eerst gecontroleerd of er een teamlid geselecteerd is
     if(!geselecteerdTeamlid) {
         return (
@@ -16,8 +58,17 @@ export default function FeedbackInvullenOverEenTeamlid({geselecteerdTeamlid}) {
             <div className={"card-body"}>
                 <h3>{geselecteerdTeamlid.naam}</h3>
                 <label className={"form-label"}>Geef feedback over {geselecteerdTeamlid.naam}</label>
-                <textarea className={"form-control"} rows={"5"}></textarea> <br />
-                <button className={"btn btn-primary"}>Verzenden</button>
+                <textarea
+                    className={"form-control"}
+                    rows={"5"}
+                    name = "commentTextArea"
+                    type="text"
+                    id="CommentsOrAdditionalInformation"
+                    value = {feedbackText}
+                    onChange={e => setFeedbackText(e.target.value)}
+                >
+               </textarea> <br />
+                <button className={"btn btn-primary"} onClick={handleSubmit}>Verzenden</button>
             </div>
         </div>
     )
