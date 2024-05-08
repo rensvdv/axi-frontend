225082a71b61a0dfa4c15a6aa31a9de8844382a3
Maissan,Casper C.A <C.Maissan@student.fontys.nl>
2024-05-08 14:02:21 +0200

    Zien welke feedback jijzelf hebt gestuurd

---
+++ src/Components/GetZenderFeedback.js
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+export default function MijnFeedbackLijst({onClick}) {
+    const [feedbackObjecten, setFeedbackObjecten] = useState([]);
+
+    useEffect(() => {
+        //Bij het laden van MijnFeedbackLijst wordt alle gekregen feedback van de gebruiker opgehaald
+        axios.get("https://localhost:7145/feedbackAPI/Feedback/getzenderfeedback/1")
+            .then(response => {
+                setFeedbackObjecten(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, );
+
+    return (
+        <div className={"list-group"}>
+            {feedbackObjecten && feedbackObjecten.map((feedback) => (
+                /*Deze onclick verwijst naar de onclick uit MijnFeedback, doordat deze als property is meegegeven */
+                <div key={feedback.id} onClick={() => onClick(feedback)}>
+                    <div className={"list-group-item list-group-item-action"} aria-current={"true"}>
+                        <div className={"d-flex justify-content-between"}>
+                            <h5 className={"mb-1"}>Zender: {feedback.zender.naam}</h5>
+                            <small>{feedback.id}</small>
+                        </div>
+                        <p className={"mb-1"}>{feedback.givenFeedback}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+
+
+    );
+}
--- src/Pages/FeedbackGeven.js
+++ src/Pages/FeedbackGeven.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import AlleTeamledenLijst from "../Components/AlleTeamledenLijst";
 import FeedbackInvullenOverEenTeamlid from "../Components/FeedbackInvullenOverEenTeamlid";
+import MijnFeedbackLijst from "../Components/MijnFeedbackLijst";
+import GetZenderFeedback from "../Components/GetZenderFeedback";
 
 
 export default function FeedbackGeven() {
@@ -14,17 +16,18 @@ export default function FeedbackGeven() {
     return (
         <div className={"container"}>
             <div className={"row"}>
-                <div className={"col-1"}></div>
                 <div className={"col-4 gy-2"}>
                     {/*De onclick wordt als property meegegeven aan AlleTeamledenLijst */}
                     <AlleTeamledenLijst onClick={handleClick} />
                 </div>
-                <div className={"col-1"}></div>
-                <div className={"col-6 gy-2"} style={{height: "90vh"}}>
+                <div className={"col-4 gy-2"} style={{height: "90vh"}}>
                     <div className={"h-100"}>
                         {/*Het geselecteerde teamlid vanuit AlleTeamledenLijst wordt meegegeven als property */}
                         <FeedbackInvullenOverEenTeamlid geselecteerdTeamlid={geselecteerdTeamlid}/>
                     </div>
+                </div>
+                <div className={"col-4 gy-2"}>
+                    <GetZenderFeedback onClick={handleClick}/>
                 </div>
             </div>
         </div>
